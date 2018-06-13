cdc0053ee369c76e321c7be9be1a8ce9f1e54f22
MelvinBusch <melvin.busch@t-online.de>
2018-06-13 15:20:39 +0200

    Waruum?!

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const Http = require("http");
 const Url = require("url");
 const Database = require("./database");
 let port = process.env.PORT || 8100;
-const server = Http.createServer();
+const server = Http.createServer((_request, _response) => {
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+});
 server.addListener("listening", () => console.log("Listening on Port: " + port));
 server.addListener("request", handleRequest);
 server.listen(port);
--- server.ts
+++ server.ts
@@ -3,7 +3,10 @@ import * as Url from "url";
 import * as Database from "./database";
 
 let port: number = process.env.PORT || 8100;
-const server: Http.Server = Http.createServer();
+const server: Http.Server = Http.createServer((_request: Http.IncomingMessage, _response: Http.ServerResponse) => {
+  _response.setHeader("Access-Control-Allow-Origin", "*");
+  _response.setHeader("content-type", "text/html; charset=utf-8");
+});
 
 server.addListener("listening", () => console.log("Listening on Port: " + port));
 server.addListener("request", handleRequest);
